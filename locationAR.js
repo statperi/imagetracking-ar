b19b82051610a50c96328a3050557458aeb300cc
Periklis <periklis.stathopoulos@voicesage.com>
2021-10-15 10:10:10 +0100

    make objects smaller

--- locationAR.js
+++ locationAR.js
@@ -5,7 +5,8 @@ window.onload = () => {
 var ar_models = [{
     code: 'pointer',
     url: './assets/map_pointer/scene.gltf',
-    scale: '5 5 5',
+    scale: '2 2 2',
+    text_scale: '50 50 50',
     rotation: '0 180 0',
     info: 'Map Pointer'
 }];
@@ -55,7 +56,7 @@ function getCoordinatesSuccess(response) {
         let newIndex = modelIndex % ar_models.length;
         let ar_model = ar_models[newIndex];
 
-        createModel(ar_model, position);
+        createModel(ar_model, position, true);
     }
 
 
@@ -63,43 +64,35 @@ function getCoordinatesSuccess(response) {
 }
 
 
-function createModel(model, location) {
+function createModel(model, location, autoscale) {
     let scene = document.querySelector('a-scene');
     let entity = document.createElement('a-entity');
-    entity.setAttribute('gps-entity-place', `latitude: ${location.latitude}; longitude: ${location.longitude};`);
 
-    setModel(model, entity);
+    entity.setAttribute('scale', model.scale);
+    entity.setAttribute('rotation', model.rotation);
+    entity.setAttribute('position', model.position);
+    entity.setAttribute('gltf-model', model.url);
+    entity.setAttribute('info', model.info);
+    entity.setAttribute('animation-mixer', '');
+    entity.setAttribute('gps-entity-place', `latitude: ${location.latitude}; longitude: ${location.longitude};`);
 
     scene.appendChild(entity);
 
 
-    let scale = model.scale.substr(0, 2); // model's scale
+    // let scale = model.scale.substr(0, 2); // model's scale
 
     let text = document.createElement('a-text');
     text.setAttribute('value', model.info);
     text.setAttribute('look-at', '[gps-camera]');
     text.setAttribute('gps-entity-place', `latitude: ${location.latitude}; longitude: ${location.longitude};`);
+    text.setAttribute('scale', model.text_scale)
 
-    scaleUp(text, scale * 10)
+    //scaleUp(text, scale * 10)
 
     scene.appendChild(text);
 
-
-    autoScale(entity, text);
+    refresh(entity, text, autoscale);
 }
-
-
-function setModel(model, entity) {
-    let element = $(entity);
-
-    if (model.scale) element.attr('scale', model.scale);
-    if (model.rotation) element.attr('rotation', model.rotation);
-    if (model.position) element.attr('position', model.position);
-
-    element.attr('gltf-model', model.url);
-    element.attr('animation-mixer', '');
-    element.attr('info', model.info);
-};
 
 
 function poolbegModel() {
@@ -107,6 +100,7 @@ function poolbegModel() {
         code: 'pointer',
         url: './assets/map_pointer/scene.gltf',
         scale: '50 50 50',
+        text_scale: '500 500 500',
         rotation: '0 180 0',
         info: 'Poolbeg'
     }
@@ -115,7 +109,7 @@ function poolbegModel() {
 }
 
 
-function autoScale(model, text) {
+function refresh(model, text, autoscale) {
     setInterval(function () {
         distance = model.getAttribute('distance');
 
@@ -123,8 +117,10 @@ function autoScale(model, text) {
             text.setAttribute('value', model.getAttribute('info') + ' - ' + Math.trunc(distance) + ' meters');
         }
 
-        let scale = model.getAttribute('scale');
-        scaleUp(model, scale.x + 2);
+        if (autoscale) {
+            let scale = model.getAttribute('scale');
+            scaleUp(model, scale.x + 0.2);
+        }
 
     }, 15000);
 }
