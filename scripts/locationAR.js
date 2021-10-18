fcd9578364b3edd19c667b63636310e3d93f4130
Periklis <periklis.stathopoulos@voicesage.com>
2021-10-18 15:20:06 +0100

    add phone booth, refactor

--- scripts/locationAR.js
+++ scripts/locationAR.js
@@ -1,43 +1,51 @@
-////isMarkerVisible = false;
-////rotationFactor = 5;
-////scaleFactor = 1;
-////minScale = 0.25;
-////maxScale = 10;
-
-
 window.onload = () => {
     getCurrentLocation();
-
-
-    // initSceneListeners();
 };
 
-var ar_models = [{
-    code: 'pointer',
-    url: './assets/map_pointer/scene.gltf',
-    scale: '1 1 1',
-    text_scale: '50 50 50',
-    rotation: '0 0 0',
-    // position: '0 30 0',
-    info: 'Pin'
-}];
+var pointer = () => {
+    return {
+        code: 'pointer',
+        url: './assets/map_pointer/scene.gltf',
+        scale: '1 1 1',
+        text_scale: '50 50 50',
+        rotation: '0 0 0',
+        // position: '0 30 0',
+        info: 'Pin'
+    }
+};
 
-var firework = {
-    code: 'firework',
-    url: './assets/firework/scene.gltf',
-    scale: '2 2 2',
-    rotation: '0 0 0',
-    // position: '0 30 0',
-    info: 'Firework'
+var firework = () => {
+    return {
+        code: 'firework',
+        url: './assets/firework/scene.gltf',
+        scale: '2 2 2',
+        rotation: '0 0 0',
+        // position: '0 30 0',
+        info: 'Firework'
+    }
 }
 
-var star = {
-    code: 'star',
-    url: './assets/star/scene.gltf',
-    scale: '2 2 2',
-    rotation: '0 0 0',
-    // position: '0 30 0',
-    info: 'Star'
+var star = () => {
+    return {
+        code: 'star',
+        url: './assets/star/scene.gltf',
+        scale: '2 2 2',
+        rotation: '0 0 0',
+        // position: '0 30 0',
+        info: 'Star'
+    }
+}
+
+var phone_booth = () => {
+    return {
+        code: 'phone_booth',
+        url: './assets/phone_booth/scene.gltf',
+        scale: '1 1 1',
+        text_scale: '50 50 50',
+        rotation: '0 0 0',
+        // position: '0 30 0',
+        info: 'Phone Booth'
+    }
 }
 
 
@@ -76,46 +84,21 @@ function processGetCoordinates(currentLocation) {
 }
 
 
-var modelIndex = 0;
 function getCoordinatesSuccess(response) {
     for (var i = 0; i < response.length; i++) {
-        modelIndex++;
 
-        let position = response[i];
-        let newIndex = modelIndex % ar_models.length;
-        let ar_model = ar_models[newIndex];
+        //let pointer = pointer();
+        //pointer.location = response[i];
+        //createEntity(pointer, true);
 
-        createModel(ar_model, position, true);
+        let phone = phone_booth();
+        phone.location = response[i];
+        createEntity(phone);
     }
 
     poolbegModel();
 }
 
-
-function createModel(model, location, autoscale) {
-    let scene = document.querySelector('a-scene');
-    
-    let entity = createModelElement({
-        url: model.url,
-        info: model.info,
-        position: model.position,
-        rotation: model.rotation,
-        scale: model.scale,
-        location: location
-    });
-    
-    let text_element = createTextElement({
-        info: model.info,
-        scale: model.text_scale,
-        location: location
-    });
-
-    scene.appendChild(entity);
-    scene.appendChild(text_element);
-
-
-    refresh(entity, text_element, autoscale);
-}
 
 function poolbegModel() {
     let poolbegModel = {
@@ -125,35 +108,65 @@ function poolbegModel() {
         text_scale: '500 500 500',
         position: '0 30 0',
         rotation: '0 180 0',
-        info: 'Poolbeg'
+        info: 'Poolbeg',
+        location: {
+            latitude: 53.3401000,
+            longitude: -6.187800
+        }
     }
 
-    createModel(poolbegModel, { latitude: 53.3401000, longitude: -6.187800 });
+    createEntity(poolbegModel);
 }
 
-function refresh(model, text, autoscale) {
+
+function createEntity(model, autoscale) {
+    let scene = document.querySelector('a-scene');
+    
+    let entityEl = createEntityElement({
+        url: model.url,
+        info: model.info,
+        position: model.position,
+        rotation: model.rotation,
+        scale: model.scale,
+        location: model.location
+    });
+    
+    let textEl = createTextElement({
+        info: model.info,
+        scale: model.text_scale,
+        location: model.location
+    });
+
+    scene.appendChild(entityEl);
+    scene.appendChild(textEl);
+
+    refresh(entityEl, textEl, autoscale);
+}
+
+
+function refresh(entity, text, autoscale) {
     setInterval(function () {
-        distance = model.getAttribute('distance');
+        distance = entity.getAttribute('distance');
 
         if (!distance)
             return;
 
-        text.setAttribute('value', model.getAttribute('info') + ' - ' + Math.trunc(distance) + ' meters');
+        text.setAttribute('value', entity.getAttribute('info') + ' - ' + Math.trunc(distance) + ' meters');
 
         if (autoscale) {
-            let scale = model.getAttribute('scale');
-            scaleUp(model, scale.x + 0.0002);
+            let scale = entity.getAttribute('scale');
+            scaleUp(entity, scale.x + 0.0002);
         }
 
         if (Math.trunc(distance) <= 5) {
-            showSuccess(model, text);
+            showSuccess(entity, text);
         }
 
     }, 1000);
 }
 
 
-function showSuccess(model, text) {
+function showSuccess(entity, text) {
     let scene = document.querySelector('a-scene');
 
     //let success = createTextElement({
@@ -162,16 +175,13 @@ function showSuccess(model, text) {
     //    location: model.getAttribute('gps-entity-place')
     //});
 
-
-    //firework.location = model.getAttribute('gps-entity-place');
-    //let success = createModelElement(firework);
-
-    star.location = model.getAttribute('gps-entity-place');
-    let success = createModelElement(star);
+    let star = star();
+    star.location = entity.getAttribute('gps-entity-place');
+    let success = createEntityElement(entity);
 
     scene.appendChild(success);
 
-    model.remove(); // remove current models
+    entity.remove(); // remove current models
     text.remove();
 }
 
@@ -181,7 +191,7 @@ function scaleUp(model, scale) {
 
 
 
-function createModelElement(config) {
+function createEntityElement(config) {
     let element = document.createElement('a-entity');
     element.setAttribute('scale', config.scale);
     element.setAttribute('rotation', config.rotation);
@@ -191,7 +201,6 @@ function createModelElement(config) {
     element.setAttribute('animation-mixer', '');
     element.setAttribute('gps-entity-place', `latitude: ${config.location.latitude}; longitude: ${config.location.longitude};`);
 
-    //gesture-handler="minScale: 0.25; maxScale: 10"
     element.setAttribute('gesture-handler', 'minScale: 0.25; maxScale: 10');
     element.classList.add('clickable');
 
@@ -206,63 +215,3 @@ function createTextElement(config) {
     element.setAttribute('gps-entity-place', `latitude: ${config.location.latitude}; longitude: ${config.location.longitude};`);
     return element;
 }
-
-
-
-
-
-
-//function initSceneListeners() {
-//    var sceneEl = $('a-scene')[0];
-
-//    sceneEl.addEventListener("markerFound", (e) => {
-//        console.log('marker is visible');
-//        isMarkerVisible = true;
-//    });
-
-//    sceneEl.addEventListener("markerLost", (e) => {
-//        console.log('marker lost');
-//        isMarkerVisible = false;
-//    });
-
-
-
-//    sceneEl.addEventListener("onefingermove", handleRotation);
-//    sceneEl.addEventListener("twofingermove", handleScale);
-//}
-
-
-//function handleRotation(event) {
-//    if (isMarkerVisible) {
-//        console.log('rotation event', event);
-
-//        $('a-entity').forEach((el) => {
-//            el.object3D.rotation.y +=
-//                event.detail.positionChange.x * rotationFactor;
-
-//            el.object3D.rotation.x +=
-//                event.detail.positionChange.y * rotationFactor;
-//        });
-//    }
-//}
-
-//function handleScale(event) {
-//    console.log('scale event', event);
-
-
-//    this.scaleFactor *=
-//        1 + event.detail.spreadChange / event.detail.startSpread;
-
-//    // gesture-handler="minScale: 0.25; maxScale: 10"
-
-//    this.scaleFactor = Math.min(
-//        Math.max(this.scaleFactor, this.minScale),
-//        this.maxScale
-//    );
-
-//    $('a-entity').forEach((el) => {
-//        el.object3D.scale.x = scaleFactor * initialScale.x;
-//        el.object3D.scale.y = scaleFactor * initialScale.y;
-//        el.object3D.scale.z = scaleFactor * initialScale.z;
-//    });
-//}
